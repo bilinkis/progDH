5314276f367f2675c4b5decb1a0ee2603d813d36
Nico Bilinkis <nico@bilinkis.com>
2020-06-30 20:34:52 -0300

    final version

--- index.js
+++ index.js
@@ -1,26 +1,23 @@
-var currentData;
+var currentData=localStorage.getItem('currentData');
 var div = document.querySelector("#number");
 var buttonSumar = document.querySelector("#sumar");
 var buttonRestar = document.querySelector("#restar");
 buttonSumar.onclick = sumar;
 buttonRestar.onclick = restar;
 function load(){
-  if(!localStorage.getItem('currentData')){
-    localStorage.setItem('currentData',0);
-    currentData = 0;
-} else{
   if (currentData == null || currentData == 0){
+    console.log(currentData)
     div.innerHTML = 0;
     currentData = 0;
 
 
   } else{
     currentData = parseInt(localStorage.getItem('currentData'));
-    div.innerHTML = currentData;
+    div.innerHTML = currentData.toString();
     console.log(currentData);
   }
 }
-}
+
 function sumar(){
   currentData += 1;
   div.innerHTML = currentData;
