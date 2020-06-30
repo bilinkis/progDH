a7ac1b1bcbf03d8dc4d59204db9fbf5257dd6e05
Nico Bilinkis <nico@bilinkis.com>
2020-06-30 20:31:27 -0300

    working

--- index.js
+++ index.js
@@ -8,16 +8,18 @@ function load(){
   if(!localStorage.getItem('currentData')){
     localStorage.setItem('currentData',0);
     currentData = 0;
-}
+} else{
   if (currentData == null || currentData == 0){
     div.innerHTML = 0;
     currentData = 0;
 
 
   } else{
+    currentData = parseInt(localStorage.getItem('currentData'));
     div.innerHTML = currentData;
     console.log(currentData);
   }
+}
 }
 function sumar(){
   currentData += 1;
