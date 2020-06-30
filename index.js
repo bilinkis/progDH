8006714d282368b3ff2fbfe53c021f036374765a
Nico Bilinkis <nico@bilinkis.com>
2020-06-30 19:46:13 -0300

    ejercicio para el final

---
+++ index.js
@@ -0,0 +1,28 @@
+var currentData = localStorage.getItem('currentData');
+var div = document.querySelector("#number");
+var buttonSumar = document.querySelector("#sumar");
+var buttonRestar = document.querySelector("#restar");
+buttonSumar.onclick = sumar;
+buttonRestar.onclick = restar;
+function load(){
+  if (currentData == null || currentData == 0){
+    div.innerHTML = 0;
+    currentData = 0;
+
+  } else{
+    div.innerHTML = currentData;
+    console.log(currentData);
+  }
+}
+function sumar(){
+  currentData += 1;
+  div.innerHTML = currentData;
+  localStorage.setItem('currentData', currentData);
+  alert('voy a sumar una unidad');
+}
+function restar(){
+  currentData -= 1;
+  div.innerHTML = currentData;
+  localStorage.setItem('currentData', currentData);
+  alert('voy a restar una unidad');
+}
